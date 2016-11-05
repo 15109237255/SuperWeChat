66477cdce2260ea78d2f420443234ce50c8ebc25
巨辰妍 <1056710249@qq.com>
2016-11-05 16:04:41 +0800

    修改头像的默认图片

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -1,11 +1,13 @@
 package com.hyphenate.easeui.utils;
 
 import android.content.Context;
+import android.support.v4.app.FragmentActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
@@ -82,10 +84,10 @@ public class EaseUserUtils {
                 Glide.with(context).load(avatarResId).into(imageView);
             } catch (Exception e) {
                 //use default avatar
-                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
+                Glide.with(context).load(user.getAvatar()).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.default_hd_avatar).into(imageView);
             }
         }else{
-            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+            Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
         }
     }
 
@@ -102,5 +104,24 @@ public class EaseUserUtils {
             }
         }
     }
-    
+
+    public static void setCuentAppUserAvatar(FragmentActivity activity, ImageView imageView) {
+        String username= EMClient.getInstance().getCurrentUser();
+        setAppUserAvatar(activity,username,imageView);
+
+    }
+
+    public static void setCuentAppUserNick(TextView tvProfileNickname) {
+        String username=EMClient.getInstance().getCurrentUser();
+        setAppUserNick(username,tvProfileNickname);
+    }
+
+    public static void setCuentAppUserNameWithNo(TextView textView) {
+        String username=EMClient.getInstance().getCurrentUser();
+        setAppUserName("微信号 :",username,textView);
+    }
+
+    private static void setAppUserName(String suffix,String username, TextView textView) {
+        textView.setText(username);
+    }
 }
